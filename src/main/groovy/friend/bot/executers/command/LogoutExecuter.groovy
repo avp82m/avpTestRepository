ba053a9844bacabf4df627a9a41171e54a9173f9
sbt-perelygin-av <sbt-perelygin-av@RTS-261015-VV1.sigma.sbrf.ru>
2018-02-05 09:00:17 +0300

    Универсализация процедуры выхода пользователя

--- src/main/groovy/friend/bot/executers/command/LogoutExecuter.groovy
+++ src/main/groovy/friend/bot/executers/command/LogoutExecuter.groovy
@@ -1,6 +1,9 @@
 package friend.bot.executers.command
 
 import java.util.HashMap
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Message
 
@@ -12,15 +15,21 @@ import friend.bot.executers.IExecuters
 class LogoutExecuter implements IExecuters {
 	private Message message	=	null;
 	private SendMessage outMessage	=	new SendMessage();
+	private static final Logger log = LoggerFactory.getLogger(LogoutExecuter.class);
 	
 	@Override
 	public void setMessage(Message message) {
 		this.message=message;
 	}
 	
+	public static FriendUser logout(Long userId) {
+		log.debug("Пользователь {} выходит",userId);
+		return CacheManager.removeUserFromCache(userId);
+	}
+	
 	@Override
 	public SendMessage getAnswer() {
-		FriendUser friendUser	=	CacheManager.removeUserFromCache(message.getFrom().getId());
+		FriendUser friendUser	=	logout(message.getFrom().getId().toLong())
 		if(friendUser	!=	null) {
 			outMessage.setText("До свидания "+friendUser.getFname()+" "+friendUser.getName()+"!");
 		}else {
